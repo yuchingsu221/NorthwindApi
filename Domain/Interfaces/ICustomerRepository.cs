fa01ec7a5bc95f3cf7cf81859a517c22e0f7ffe3
2100867A <williamsu@softmobile.com.tw>
2024-05-20 17:14:30 +0800

    [add] 加入 CustomerRepository、ICustomerRepository

---
+++ Domain/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using NorthwindApi.Domain.Entities;
+
+namespace NorthwindApi.Domain.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task<IEnumerable<Customer>> GetAllAsync();
+        Task<Customer> GetByIdAsync(string id);
+        Task AddAsync(Customer customer);
+        Task UpdateAsync(Customer customer);
+        Task DeleteAsync(string id);
+    }
+}
---
+++ Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NorthwindApi.Domain.Entities;
+using NorthwindApi.Domain.Interfaces;
+using NorthwindApi.Infrastructure.Context;
+
+namespace NorthwindApi.Infrastructure.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly NorthwindContext _context;
+
+        public CustomerRepository(NorthwindContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Customer>> GetAllAsync()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+
+        public async Task<Customer> GetByIdAsync(string id)
+        {
+            return await _context.Customers.FindAsync(id);
+        }
+
+        public async Task AddAsync(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Customer customer)
+        {
+            _context.Entry(customer).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
