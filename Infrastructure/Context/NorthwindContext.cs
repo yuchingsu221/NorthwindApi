ed632a8952f6554d2201a09eb68807308efa06b4
2100867A <williamsu@softmobile.com.tw>
2024-05-20 16:32:37 +0800

    [add] 加入 DbContext

---
+++ Infrastructure/Context/NorthwindContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindApi.Domain.Entities;
+using System.Collections.Generic;
+
+namespace NorthwindApi.Infrastructure.Context
+{
+    public class NorthwindContext : DbContext
+    {
+        public NorthwindContext(DbContextOptions<NorthwindContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
