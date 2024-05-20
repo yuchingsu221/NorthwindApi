4f5e17ff4558f228cdcffb1ba7007b8e7fa52d9b
2100867A <williamsu@softmobile.com.tw>
2024-05-20 17:15:57 +0800

    [add] 加入 CustomersController、CustomerService

---
+++ Services/CustomerService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using NorthwindApi.Domain.Entities;
+using NorthwindApi.Domain.Interfaces;
+
+namespace NorthwindApi.Services
+{
+    public class CustomerService
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerService(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<IEnumerable<Customer>> GetAllCustomersAsync()
+        {
+            return await _customerRepository.GetAllAsync();
+        }
+
+        public async Task<Customer> GetCustomerByIdAsync(string id)
+        {
+            return await _customerRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddCustomerAsync(Customer customer)
+        {
+            await _customerRepository.AddAsync(customer);
+        }
+
+        public async Task UpdateCustomerAsync(Customer customer)
+        {
+            await _customerRepository.UpdateAsync(customer);
+        }
+
+        public async Task DeleteCustomerAsync(string id)
+        {
+            await _customerRepository.DeleteAsync(id);
+        }
+    }
+}
